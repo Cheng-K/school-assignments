a239b16ebae05632be489afde1b561f434ffa7d0
Cheng-K <adamkei2612@gmail.com>
2021-04-27 21:50:21 +0800

    Menu V1.0
    Complete design student menu & half way done for admin menu

---
+++ SportsCenterApp/src/com/company/AdminMenu.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import javax.swing.*;
+
+public class AdminMenu {
+    private JButton createNewRecordButton;
+    private JButton showRecordsButton;
+    private JButton searchRecordButton;
+    private JButton modifyExistingRecordButton;
+    private JButton signOutButton;
+    private JButton displaySportCenterDetailsButton;
+}
---
+++ SportsCenterApp/src/com/company/StudentMenu.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import javax.swing.*;
+
+public class StudentMenu {
+    private JLabel welcomeHeading;
+    private JButton viewSportsDetailsButton;
+    private JButton personalProfileButton;
+    private JButton coachDetailsButton;
+    private JButton sportScheduleButton;
+    private JLabel secondHeading;
+    private JPanel studentMenuPanel;
+    private JButton signOutButton;
+    // private Student studentA ;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
