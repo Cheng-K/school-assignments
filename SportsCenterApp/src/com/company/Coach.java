dbfa527f03acc29f332ee95fcb25b99dc58efd85
JakeKrong <kzkong14@gmail.com>
2021-04-27 17:38:50 +0800

    Created loginScreen, student, coach class

---
+++ SportsCenterApp/src/com/company/Coach.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Coach {
+    String coachID;
+    String name;
+    String dateJoined;
+    String dateTerminated;
+    int hourlyRate;
+    int phone;
+    String address;
+    String sportsCenterID;
+    String sportsCode;
+    int rating;
+}
--- SportsCenterApp/src/com/company/Student.java
+++ SportsCenterApp/src/com/company/Student.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Student {
+    String name;
+    String studentID;
+    int age;
+    String address;
+    int contactNumber;
+    String email;
+    String registeredSports;
+    String sportsCenterID;
 }
