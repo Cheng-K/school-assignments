cc268bc07aa8d1241fb3358672e1cdd081fc3b67
Cheng-K <adamkei2612@gmail.com>
2021-05-03 22:06:27 +0800

    Done Menu for guest Student
    Unregistered Student should have only three option in the menu.
    
    3/5/2021

--- SportsCenterApp/src/com/company/StudentMenu.java
+++ SportsCenterApp/src/com/company/StudentMenu.java
@@ -12,31 +12,56 @@ public class StudentMenu {
     private JLabel secondHeading;
     private JPanel studentMenuPanel;
     private JButton signOutButton;
-    private Student studentA ;
+    private JPanel regStudentButtonPanel;
+    private JButton registerButton;
+    private JPanel guestButtonPanel;
+    private Student regStudent;
     private UnregStudent guestStudent;
 
     /*  Description : Constructor that creates studentMenu frame
-        Parameter   : Student object (current user)
+        Parameter   : BaseStudent object (current user)
         Output      : Menu Screen
      */
     public StudentMenu (BaseStudent studentA){
         frame = new JFrame("Main Menu");
-
         if (studentA instanceof Student) {
-            welcomeHeading.setText("Welcome, " + ((Student) studentA).getName());
-            this.studentA = (Student) studentA;
+            regStudent = (Student) studentA;
             guestStudent = null;
         }
         else{
-            welcomeHeading.setText("Welcome, Guest 001");
             guestStudent = (UnregStudent) studentA;
-            studentA = null;
+            regStudent = null;
         }
+        setWelcomeHeading();
+        setVisibleButtons();
         frame.setContentPane(studentMenuPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
+    }
+
+    /*
+        Method name : setVisibleButtons
+        Description : Sets which buttons that can be viewed by registered and non-registered student
+     */
+
+    public void setVisibleButtons () {
+        if (guestStudent != null)
+            regStudentButtonPanel.setVisible(false);
+        else
+            guestButtonPanel.setVisible(false);
+    }
+
+    /*
+        Method name : setWelcomeHeading
+        Description : Sets welcome message according to type of student (registered/unregistered)
+     */
+    public void setWelcomeHeading(){
+        if (guestStudent != null)
+            welcomeHeading.setText("Welcome, Guest 001");
+        else
+            welcomeHeading.setText("Welcome, "+ regStudent.getName());
     }
 
 }
