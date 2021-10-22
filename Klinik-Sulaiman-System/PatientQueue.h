8f29adeb702603f1d4d58871098089977bf18f4f
JakeKrong <kzkong14@gmail.com>
2021-10-22 21:21:07 +0800

    22/10
    Bruh

--- Klinik-Sulaiman-System/PatientQueue.h
+++ Klinik-Sulaiman-System/PatientQueue.h
@@ -1,6 +1,7 @@
 #pragma once
 # include "LinkedList.h";
 # include "Node.h";
+#include <string>
 
 
 class PatientQueue : public LinkedList {
@@ -25,4 +26,75 @@ public :
 			//append(patient)
 		}
 	}
+
+    Patient getNextPatient()
+    {
+        Node* current;
+        std::string input;
+        if (head == NULL)
+        {
+            std::cout << "Patient queue is already empty, there are no patients to call." << std::endl;
+            std::cout << "Press 'enter' to continue";
+            getline(std::cin, input);
+            return;
+        }
+        current = head;
+        if (head == lastDisabledPatient)
+        {
+            lastDisabledPatient = NULL;
+        }
+        head = head->getNextNode();
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+        else
+        {
+            *head->getPreviousNode() = NULL;
+        }
+        return current->getPatient();
+    }
+
+    int removePatient(std::string patientID)
+    {
+        Node* current;
+        if ((head->getPatient().getPatientID())==patientID)
+        {
+            current = head;
+            head = head->getNextNode();
+            *head->getPreviousNode() = NULL;
+            if (current = lastDisabledPatient)
+            {
+                lastDisabledPatient = NULL;
+            }
+            delete current;
+            return 1;
+        }
+        else
+        {
+            *current->getPreviousNode() = *head;
+            current = head->getNextNode();
+            while (current != NULL)
+            {
+                if (current->getPatient().getPatientID() == patientID)
+                {
+                    *current->getPreviousNode()->getNextNode() = *current->getNextNode();
+                    *current->getNextNode()->getPreviousNode() = *current->getPreviousNode();
+                    if (current == lastDisabledPatient)
+                    {
+                        lastDisabledPatient = current->getPreviousNode();
+                    }
+                    delete current;
+                    return 1;
+                }
+                else
+                {
+                    *current->getPreviousNode() = *current;
+                    current = current->getNextNode();
+                }
+            }
+            return 0;
+        }
+    }
+
 };
