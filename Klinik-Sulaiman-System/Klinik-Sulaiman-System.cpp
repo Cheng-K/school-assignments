6d590159d8fbee6c467708eab9fe094854b6dd44
Cheng-K <adamkei2612@gmail.com>
2021-11-01 15:24:37 +0800

    Added some login interface and doctor flow of program

--- Klinik-Sulaiman-System/Klinik-Sulaiman-System.cpp
+++ Klinik-Sulaiman-System/Klinik-Sulaiman-System.cpp
@@ -12,9 +12,59 @@
 
 
 
-int main()
-{
+int main() {
+	PatientQueue* p = new PatientQueue();
+	Doctor* doctor = new Doctor(p);
+	Nurse* nurse = new Nurse(p);
+	std::string username;
+	std::string password;
+	bool loginAsDoctor = false;
+	int loginStatus = 0;
+	while (true) {
+		std::cout << "Login Page" << std::endl;
+		std::cout << "Username : ";
+		getline(std::cin, username);
+		std::cout << "Password : ";
+		password = Utility::getPassword();
+		//loginStatus = login(username,password,loginAsDoctor);
+		system("cls");
+		if (loginStatus == -1) {
+			std::cout << "Login failed. Invalid credentials provided. Please try again." << std::endl;
+			system("pause");
+		}
+		// Doctor
+		else if (loginStatus == 0) {
+			char choice = 0;
+			while (true) {
+				Utility::displayDoctorMenu();
+				std::cin >> choice;
+				cin.ignore(256, '\n');
+				if (choice == '1')
+					doctor->viewInfo();
+				else if (choice == '2') {
+					// search and modify patients ("treat patient")
+				}
+				else if (choice == '3') {
+					// search patient
+				}
+				else {
+					break;
+				}
+			}
+		}
+		else {
+			//nurse
+			while (true) {
 
+			}
+		}
+		system("cls");
+	}
+	return 0;
+
+}
+
+int test() {
 	PatientQueue* p = new PatientQueue();
 	Doctor* d = new Doctor(p);
 	Nurse* n = new Nurse(p);
@@ -22,7 +72,7 @@ int main()
 		Patient* a;
 		/*a = n->createPatient();*/
 		if (i % 2 == 0)
-		a = new Patient("a" + std::to_string(i), "a", "a", "a", "a","a", true);
+			a = new Patient("a" + std::to_string(i), "a", "a", "a", "a", "a", true);
 		else
 			a = new Patient("a" + std::to_string(i), "a", "a", "a", "a", "a", false);
 		n->addPatient(a);
@@ -30,7 +80,7 @@ int main()
 	p->displayInQueue();
 	n->deletePatient();
 	p->displayInQueue();
-	
-	//d->viewInfo();
-}
 
+	d->viewInfo();
+	return 0;
+}
--- Klinik-Sulaiman-System/Utility.h
+++ Klinik-Sulaiman-System/Utility.h
@@ -1,7 +1,10 @@
 # pragma once
 # include <iostream>
+# include <string>
+# include <conio.h>
 # include "LinkedList.h"
 
+using namespace std;
 class Utility {
 public :
 	static LinkedList* mergeSort(LinkedList* linkedList, int searchMode) {
@@ -89,5 +92,39 @@ public :
 
 	static void viewPatient() {
 		std::cout << "Inside view Patient Method" << std::endl;
+	}
+
+	/*
+		Method : getPassword
+		Parameter : None
+		Description : Prompts user to enter password and masks the password input with * and returns the password as string.
+	*/
+
+	static std::string getPassword() {
+		std::string password;
+		char lastChar = ' ';
+		while (lastChar != 13) {
+			lastChar = _getch();
+			if (lastChar != 13 && lastChar != '\b') {
+				std::cout << '*';
+				password += lastChar;
+			}
+			else if (lastChar == '\b' && password.length() > 0) {
+				std::cout << "\b \b";
+				password.pop_back();
+			}
+		}
+		std::cout << "Password is : " << password << std::endl;
+		system("pause"); // testing
+		return password;
+	}
+
+	static void displayDoctorMenu() {
+		std::cout << "Main menu " << std::endl;
+		std::cout << "1. View Patient History List and Patient Waiting List" << std::endl;
+		std::cout << "2. Treat Patients" << std::endl;
+		std::cout << "3. Search Patients" << std::endl;
+		std::cout << "Press any key to logout..." << std::endl;
+		std::cout << "Select one of the options displayed above by entering the number : ";
 	}
 };
